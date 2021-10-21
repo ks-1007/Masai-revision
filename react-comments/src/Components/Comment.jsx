4d0f0af88d4da38a54325e21ec59c6c07ec33a65
Kumar Saurav <krsaurav1007@gmail.com>
2021-10-21 17:58:49 +0530

    work done

--- react-comments/src/Components/Comment.jsx
+++ react-comments/src/Components/Comment.jsx
@@ -3,13 +3,27 @@ import React, { useState } from "react"
 import styles from "./Comment.module.css"
 function Comment({ author, body, timestamp, points, replies }) {
   const [showReply, setShowReply] = useState(false)
-  const [replyList, setReplyList] = useState(replies)
-  console.log("replyList:", replyList)
+  const [replyList, setReplyList] = useState(replies || [])
+  const [input, setInput] = useState("")
+  const [replyInput, setReplyInput] = useState(false)
+  const handleReply = () => {
+    if (replyInput !== "") {
+      let reply = {
+        author,
+        points,
+        timestamp: new Date(),
+        body: input,
+      }
+      setReplyList([...replyList, reply])
+    }
+  }
   return (
     <div className={styles.mainCont}>
       <div className={styles.topInfo}>
-        {replyList?.length && !showReply && (
+        {replyList.length && !showReply ? (
           <h3 onClick={() => setShowReply(true)}>+</h3>
+        ) : (
+          ""
         )}
         <p onClick={() => setShowReply(false)}>{author}</p>
         <p>{points} points</p>
@@ -17,12 +31,24 @@ function Comment({ author, body, timestamp, points, replies }) {
       </div>
       <h3>{body}</h3>
       <div className={styles.options}>
-        <p>Reply</p>
+        <p onClick={() => setReplyInput(!replyInput)}>Reply</p>
         <p>Give Award</p>
         <p>Share</p>
         <p>Report</p>
         <p>Save</p>
       </div>
+      {replyInput && (
+        <div className={styles.inputCont}>
+          <input
+            type="text"
+            name=""
+            id=""
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={handleReply}>add comment</button>
+        </div>
+      )}
+
       {showReply && replyList && replyList.map((item) => <Comment {...item} />)}
     </div>
   )
